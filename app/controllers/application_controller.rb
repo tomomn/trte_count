172cfe5e7b8448c3c7bb639b8cfc611ebc8b2272
yoshimura <tech-camp@tech-camp-1-017-no-MacBook-Air.local>
2016-11-04 21:20:02 +0900

    basic ninsyo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
 
 
-  ##BASIC認証
-# http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+  #BASIC認証
+http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
   
   protect_from_forgery with: :exception
 end
