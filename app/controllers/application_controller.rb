6408ef0abcd55b78d7fde4b38c7adb38683e29b9
yoshimura <tech-camp@tech-camp-1-017-no-MacBook-Air.local>
2016-11-04 20:12:22 +0900

    basic kaizyo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
+
+
+  ##BASIC認証
+# http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "production"
   
   protect_from_forgery with: :exception
 end
